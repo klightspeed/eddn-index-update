b8113e86d3119792772b152077eb6ffd20917414
Ben Peddell <klightspeed@killerwolves.net>
2023-06-19 09:14:21 +1000

    Fix actual issue in insert

--- eddn-index-update.py
+++ eddn-index-update.py
@@ -2266,11 +2266,11 @@ class EDDNSysDB(object):
         )
 
     def add_edsm_file_line_systems(self, linelist):
-        values = [(fileid, lineno, edsmsysid, sysid, timestamp.isoformat(sep=' '), hascoords, coordslocked) for fileid, lineno, edsmsysid, sysid, timestamp, hascoords, coordslocked in linelist]
+        values = [(fileid, lineno, edsmsysid, sysid, timestamp, hascoords, coordslocked) for fileid, lineno, edsmsysid, sysid, timestamp, hascoords, coordslocked in linelist]
         self.conn.cursor().executemany(
             'INSERT INTO EDSMFileLineSystems ' +
             '(FileId, LineNo, EdsmSystemId, SystemId, Timestamp, HasCoords, CoordsLocked) VALUES ' +
-            '(%s,     %s,     %s)',
+            '(%s,     %s,     %s,           %s,       %s,        %s,        %s)',
             values
         )
 
